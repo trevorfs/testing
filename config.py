fe4918723ef153fcae2a1852d987e6c496e5ba8a
Lawvan <36541416+trevorfs@users.noreply.github.com>
2022-10-13 14:53:41 +0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+# MENGGUNAKAN CAPTCHA : anycaptcha / anti-captcha
+CAPTCHA_PROVIDER = "anti-captcha"
+# ANTI CAPTCHA KEY
+ANTI_CAPTCHA_KEY = "d6f4a3f505d57f9229b51fe4b2d45ad5"
+# ANY CAPTCHA KE
+ANY_CAPTCHA_KEY = "xx"
+# COIN
+COIN = "trx"
+COUNTDOWN = 600 # countdown / waktu mundur / jeda claim / satuan detik
